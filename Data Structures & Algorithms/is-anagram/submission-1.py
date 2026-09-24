f71e9dc5b89e405df896e82b0357e12503b81ce2
Andrew Dagher <andrewdagher18@gmail.com>
2026-09-23 18:43:37 -0600

    Add: is-anagram - submission-1

---
+++ Data Structures & Algorithms/is-anagram/submission-1.py
@@ -0,0 +1,25 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        letters1 = {}
+        letters2 = {}
+        if len(s) == len(t):
+            # i = 0, len(s) = 4
+            # 
+            for i in range(len(s)):
+                if s[i] in letters1:
+                    letters1[s[i]] += 1
+                else:
+                    letters1[s[i]] = 1
+            for i in range(len(t)):
+                if t[i] in letters2:
+                    letters2[t[i]] += 1
+                else:
+                    letters2[t[i]] = 1
+            if letters1 == letters2:
+                return True
+            else:
+                return False
+        else:
+            return False
+        
+        
